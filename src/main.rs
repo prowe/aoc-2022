cc9c03d3ca2a2482d2d05d9ac54b5370013db77f
Paul Rowe <prowe@sourceallies.com>
2022-12-01 10:40:36 -0600

    day 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, Read};
+
+use crate::day_01::day_01;
+// use std::env;
+
+mod add;
+mod day_01;
+
+
+fn main() -> io::Result<()>{
+    // let args: Vec<String> = env::args().collect();
+
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer)?;
+
+    let result = day_01(buffer);
+    println!("Result: {}", result);
+    
+    Ok(())
 }
